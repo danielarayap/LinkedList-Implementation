01de269b0c17a44b4cf3e2de5fef7e7380caffbe
danielarayap1 <daniel.araya@ing.uchile.cl>
2017-09-18 19:26:47 -0300

    Hola

---
+++ test/LinkListTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Daniel Araya on 18-09-2017.
+ */
+public class LinkListTest {
+    private IlinkList l;
+
+    @Before
+    public void setUp() throws Exception {
+        l = new LinkList();
+    }
+
+
+}
